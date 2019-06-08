0e07fe95e93ea69ba0cd08972163e567dd2fd277
Farhan Khursheed <41877347+farhan098@users.noreply.github.com>
2019-06-08 12:30:27 -0700

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,10 @@
+if (document.getElementById("react-select-6--value-item")) {a=document.getElementById("react-select-6--value-item").innerHTML;} else {a="";}
+if (document.getElementById("react-select-8--value-item")) {b=document.getElementById("react-select-8--value-item").innerHTML;} else {b="";}
+if (document.getElementById("react-select-10--value-item")) {c=document.getElementById("react-select-10--value-item").innerHTML;} else {c="";}
+if (document.getElementById("react-select-12--value-item")) {d=document.getElementById("react-select-12--value-item").innerHTML;} else {d="";}
+if (document.getElementById("react-select-14--value-item")) {e=document.getElementById("react-select-14--value-item").innerHTML;} else {e="";}
+if (document.getElementById("react-select-16--value-item")) {f=document.getElementById("react-select-16--value-item").innerHTML;} else {f="";}
+if (document.getElementById("react-select-18--value-item")) {g=document.getElementById("react-select-18--value-item").innerHTML;} else {g="";}
+if (document.getElementById("react-select-20--value-item")) {h=document.getElementById("react-select-20--value-item").innerHTML;} else {h="";}
+
+input_58_469.value=+document.getElementById("react-select-2--value-item").innerHTML+ +a+ +b+ +c+ +d+ +e+ +f+ +g+ +h;
